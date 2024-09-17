ee6aedbee60bd73b8a453903576c4ca349cedd24
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2024-09-17 12:02:00 +0300

    feat: create structure and header part for visual(info, filter, search panel and one item)

---
+++ my-app/src/components/app/app.js
@@ -0,0 +1,20 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesListItem from '../employees-list-item/employees-list-item';
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+        <EmployeesListItem />
+      </div>
+    </div>
+  );
+}
+export default App;
